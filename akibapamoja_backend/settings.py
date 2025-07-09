2e29fbf84319940f815ad1af36ea3e8ca8fa7c6b
Fortune <fortune@localhost.localdomain>
2025-07-09 21:27:21 +0300

    user model

--- akibapamoja_backend/settings.py
+++ akibapamoja_backend/settings.py
@@ -17,7 +17,7 @@ import os
 
 load_dotenv()
 
-# AUTH_USER_MODEL = 'users.CustomUser'
+AUTH_USER_MODEL = 'users.CustomUser'
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'dev-secret-key-1234567890'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
@@ -56,7 +56,7 @@ INSTALLED_APPS = [
     # 'api',
     # 'corsheaders',
     'rest_framework',
-    # 'users',
+    'users',
     # 'transactions',
     # 'reports',
     # 'notifications',
